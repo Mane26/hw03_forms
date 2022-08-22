80de13e3e39dc7fe4e469fdcfdd9159718384f33
Meline Sarkisyan <s.m.m21-08-90@mail.ru>
2022-08-22 22:44:37 +0300

    Доработка сайта

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -9,7 +9,7 @@ POSTS_NUMB = 10
 
 
 def group_posts(request, slug):
-    # Функция group_posts передает данные в шаблон group_list.html
+    """Функция group_posts передает данные в шаблон group_list.html."""
     group = get_object_or_404(Group, slug=slug)
     post_list = (group.posts.select_related("author").
                  order_by('-pub_date'))
@@ -24,14 +24,14 @@ def group_posts(request, slug):
 
 
 def index(request):
-    # Функция index передает данные в шаблон index.html
-    # Паджинация 10 постов на страницу
+    """Функция index передает данные в шаблон index.html."""
+    """Паджинация 10 постов на страницу."""
     post_list = (Post.objects.select_related("author").
                  order_by('-pub_date'))
     paginator = Paginator(post_list, POSTS_NUMB)
     page_number = request.GET.get('page')
     page_obj = paginator.get_page(page_number)
-    # Здесь код запроса к модели и создание словаря контекста
+    """Здесь код запроса к модели и создание словаря контекста."""
     context = {
         'page_obj': page_obj,
     }
@@ -39,14 +39,14 @@ def index(request):
 
 
 def profile(request, username):
-    # Здесь код запроса к модели и создание словаря контекста
+    """Здесь код запроса к модели и создание словаря контекста."""
     author = get_object_or_404(User, username=username)
-    # В тело страницы выведен список постов
+    """В тело страницы выведен список постов."""
     post_list = (author.posts.select_related("author").
                  order_by('-pub_date'))
-    # Выведено общее количество постов пользователя
+    """Выведено общее количество постов пользователя."""
     count = author.posts.count()
-    # Паджинация 10 постов на страницу
+    """Паджинация 10 постов на страницу."""
     paginator = Paginator(post_list, POSTS_NUMB)
     page_number = request.GET.get('page')
     page_obj = paginator.get_page(page_number)
@@ -60,16 +60,16 @@ def profile(request, username):
 
 
 def post_detail(request, post_id):
-    # Здесь код запроса к модели и создание словаря контекста
+    """Здесь код запроса к модели и создание словаря контекста."""
     post = get_object_or_404(Post, pk=post_id)
-    # В тело страницы выведен один пост, выбранный по pk
+    """В тело страницы выведен один пост, выбранный по pk."""
     context = {'post': post, }
     return render(request, 'posts/post_detail.html', context)
 
 
 @login_required
 def post_create(request):
-    # Добавлена "Новая запись" для авторизованных пользователей
+    """Добавлена "Новая запись" для авторизованных пользователей."""
     if request.method == "POST":
         form = PostForm(request.POST or None)
         if form.is_valid():
@@ -84,10 +84,10 @@ def post_create(request):
 
 @login_required
 def post_edit(request, post_id):
-    # Добавлена страница редактирования записи
-    # Права на редактирование должны быть только у автора этого поста
-    # Остальные пользователи должны перенаправляться
-    # На страницу просмотра поста
+    """Добавлена страница редактирования записи."""
+    """Права на редактирование должны быть только у автора этого поста."""
+    """Остальные пользователи должны перенаправляться."""
+    """На страницу просмотра поста."""
     post = Post.objects.get(pk=post_id)
     form = PostForm(None, instance=post)
     if request.user != post.author:
